5f914c316e4d92990a83d4808dbbc8c8a58639b2
X.Y.Zhang <zxy199803@gmail.com>
2021-03-19 23:19:44 +0800

    3.14

--- part4-面向对象惯用法/chapter9-符合Python风格的对象/9.2再谈向量类.py
+++ part4-面向对象惯用法/chapter9-符合Python风格的对象/9.2再谈向量类.py
@@ -3,11 +3,20 @@ import math
 
 
 class Vector2d:
+    __slots__ = ('__x','__y')  # 告诉解释器类中的所有实例属性。大规模数据应使用Numpy
     typecode = 'd'  # 类属性,在Vector2d实例和字节序列之间转换时使用
 
     def __init__(self, x, y):
-        self.x = float(x)  # 把x,y尽早转换成浮点数，尽早捕获错误
-        self.y = float(y)
+        self.__x = float(x)  # 把x,y尽早转换成浮点数，尽早捕获错误
+        self.__y = float(y)  # 使用两个前导下划线把属性标记为私有的
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property  # 把读值方法标记为特性
+    def y(self):  # 读值方法与公开属性同名
+        return self.__y
 
     def __iter__(self):
         return (i for i in (self.x, self.y))  # 将Vector2d实例变成可迭代对象，这样才能拆包
@@ -31,12 +40,32 @@ class Vector2d:
 
     def __bool__(self):
         return bool(abs(self))  # 非零值是True
+
     # 备选构造方法
     @classmethod
     def frombytes(cls, octets):  # 不用传入self参数，通过cls传入类本身
         typecode = chr(octets[0])  # 第一个字节读取tpyecode
         memv = memoryview(octets[1:].cast(typecode))  # memoryview.cast 用不同的方式读写同一块内存数据，内容字节不会随意移动
         return cls(*memv)  # 使用cls参数构建了一个新的实例
+
+    def angle(self):
+        return math.atan2(self.y, self.x)
+
+    def __format__(self, fmt_spec=''):
+        if fmt_spec.endswith('p'):
+            fmt_spec = fmt_spec[:-1]  # 从中删除p后缀
+            coords = (abs(self), self.angle())
+            outer_fmt = '<{},{}>'
+        else:
+            coords = self
+            outer_fmt = '({},{})'
+
+        components = (format(c, fmt_spec) for c in coords)
+        return outer_fmt.format(*components)
+
+    # 将对象可散列
+    def __hash__(self):
+        return hash(self.x) ^ hash(self.y)  # 使用位运算符异或(^)混合各分量的散列值
 
 
 v1 = Vector2d(1, 2)
@@ -46,4 +75,3 @@ print(x)
 print(x, y)
 print(repr(v1))
 print(bytes(v1))  # 实例的二进制表示形式
-
--- part4-面向对象惯用法/chapter9-符合Python风格的对象/9.5格式化显示.py
+++ part4-面向对象惯用法/chapter9-符合Python风格的对象/9.5格式化显示.py
@@ -4,8 +4,21 @@ format()函数和str.format()方法把各类型的格式化方法委托给相应
 format_spec是格式说明符
     是format(my_obj,format_spec)的第二个参数，或
     str.format方法的格式字符串，{}里代换字段中冒号后面的部分
+
+{0.mass:5.3e}包含两部分，字段名0.mass，格式说明符5.3e（格式规范微语言）
+
+格式规范微语言
+b 二进制int
+x 十六进制int
+f 小鼠形式的float
+格式规范微语言是可扩展的，各个类可自行决定如何解释format_spec参数。如果类没有定义__format__方法，从object继承的方法会返回str(my_object)
 """
 blr = 1/2.43
 print(blr)
 print(format(blr,'0.4f'))
 print('1 BRL = {rate:0.2f} USD'.format(rate=blr))  # 代换字段中的'rate'字串是字段名称，它决定把.format()的哪个参数传给代换字段
+
+# 可扩展的格式规范微语言
+from datetime import datetime
+now = datetime.now()
+print(format(now,'%H:%M:%S'))
